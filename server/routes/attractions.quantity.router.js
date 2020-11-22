a034fb94ae99f37c508e156d346cef1fafc3df65
Adam Boerhave <arboerhave@gmail.com>
2020-11-22 11:39:50 -0600

    authorization added to put to increment attraction count

--- server/routes/attractions.quantity.router.js
+++ server/routes/attractions.quantity.router.js
@@ -51,7 +51,7 @@ router.post('/', (req, res) => {
         and user_id = $2;`;
         pool.query(authorizationCheckText, [req.body.parkVisitId, req.user.id]).then((authResult) => {
             const checkedParkVisitId = authResult.rows[0].id;
-            console.log('post new attraction result.rows[0]', authResult.rows[0]);
+            console.log('post new attraction result.rows[0]', authResult.rows[0].id);
             
             let queryText = `insert into visits_attractions (park_visit_id, attractions_id)
             values ($1, $2) returning park_visit_id;`;
@@ -61,16 +61,16 @@ router.post('/', (req, res) => {
                 console.log('second post new attraction result.rows', result.rows[0]);
                 
                 res.send(result.rows[0]);
-            }).catch((error) => {
+            }).catch((error) => {   // catch for second query
                 console.log('error in post attractions to park visit quantity', error);
                 res.sendStatus(500);
             });
-        }).catch((error) => {
+        }).catch((error) => {   // catch for first query
             console.log('error in post attractions to park visit quantity', error);
-            
+            res.sendStatus(500);
         })
-        }   // end authentication
-    }); // end post route
+    }   // end authentication
+}); // end post route
     
     // put route to increment the number of times the attraction has been ridden
     router.put('/', (req, res) => {
@@ -81,20 +81,34 @@ router.post('/', (req, res) => {
         res.sendStatus(403);
     }
     else {  // authenticated
-        let queryText = `update visits_attractions
-        set times_ridden = times_ridden + 1
-        where park_visit_id = $1
-        and attractions_id = $2
-        returning park_visit_id;`;
 
-        pool.query(queryText, [req.body.parkVisitId, req.body.attraction]).then((result) => {
-            console.log('successfully incremented attraction in park visit');
-            res.send(result.rows[0])
-        
-        }).catch((error) => {
+        let authorizationCheckText = `select id from park_visits
+        where id = $1
+        and user_id = $2;`;
+
+        pool.query(authorizationCheckText, [req.body.parkVisitId, req.user.id]).then((authResult) => {
+            const checkedParkVisitId = authResult.rows[0].id;
+            console.log('increment attraction checkedParkVisitId', checkedParkVisitId);
+
+            
+            let queryText = `update visits_attractions
+            set times_ridden = times_ridden + 1
+            where park_visit_id = $1
+            and attractions_id = $2
+            returning park_visit_id;`;
+            
+            pool.query(queryText, [checkedParkVisitId, req.body.attraction]).then((result) => {
+                console.log('successfully incremented attraction in park visit');
+                res.send(result.rows[0])
+                
+            }).catch((error) => {   // catch for second query
+                console.log('error in attractionsQuantity put', error);
+                res.sendStatus(500);
+            });
+        }).catch((error) => {   // catch for first query
             console.log('error in attractionsQuantity put', error);
             res.sendStatus(500);
-        });
+        })
     }   // end authentication
 })  // end put route
 
