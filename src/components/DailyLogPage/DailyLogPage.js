a6a33c08af12a1a3e0b06334f36dbbbc4be2015f
Adam Boerhave <arboerhave@gmail.com>
2020-11-27 18:48:48 -0600

    comments in attractions page, date selection page, parks page, some of daily log page

--- src/components/DailyLogPage/DailyLogPage.js
+++ src/components/DailyLogPage/DailyLogPage.js
@@ -1,26 +1,39 @@
+// DailyLogPage component for Solo Project App for Prime Academy
+// created by Adam Boerhave, November 2020
+// User can arrive here after selecting a date from the dateSelectionpage or 
+// after clicking edit for a previously saved visit.  They have the ability
+// to increment attraction that have been experience here, and add additional 
+// notes.  When finished, they may complete the visit to remove editing capabilities 
+// for the future
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
 
 class DailyLogPage extends Component {
+    // local state for notes and logId used to save notes
     state = {
         notes: '',
         logId: 0
     };
 
     componentDidMount = () => {
+        // get park user is storing visit for
         this.props.dispatch({type: 'GET_VISIT_PARK', payload: this.props.match.params.id})
-        
+        // get user's saved favorites
         this.props.dispatch({type: 'GET_FAVORITES'});
+        // get user's previously saved favorites that have been turned off
         this.props.dispatch({type: 'GET_OFF_FAVORITES'});
         // get the attractions at the chosen park
         this.props.dispatch({type: 'GET_ATTRACTIONS_FOR_PARK_VISIT', payload: this.props.match.params.id});
+        // get the number of times the user has already done any of these attractions
         this.props.dispatch({type: 'GET_ATTRACTIONS_QUANTITY', payload: this.props.match.params.id});
+        // get the notes the user has alread saved
         this.props.dispatch({type: 'GET_NOTES', payload: this.props.match.params.id});
     }
 
-    
+    // ???????????????????????????????????????????????????????????????????????????????????
     componentDidUpdate = () => {
         if (this.props.store.singleVisit !== this.state.logId) {
             console.log('state', this.state);
@@ -32,6 +45,8 @@ class DailyLogPage extends Component {
         }
     }
 
+    // This render section works similarly to the render section from the AttractionsPage component
+    // Attractions get an orange heart if currently saved as a favorite and a white heart otherwise
     renderFavorite = (attractionId) => {
         // check to see if this number that is the attractionId is
         // in the list of attractionIds in the favorite table
@@ -47,29 +62,26 @@ class DailyLogPage extends Component {
         }
     }
 
+    // toggle the favorite_status to false if the attraction is currently on and clicked
     handleClickOff = (attractionId) => {
-        console.log('clicked', attractionId);
-        console.log('option1');
         this.props.dispatch({type: 'TOGGLE_FAVORITE', payload: attractionId});
     }
 
+    // make the favorite_status true for the attraction
     handleClickOn = (attractionId) => {
-        console.log('clicked', attractionId);
         // it should go to this section if the user has previously set it as a favorite
         // and turned it off again so that it exists in the favorites table
-        if(this.props.store.offFavorites.some(attraction => attraction.attraction_id === attractionId)){
-            console.log('option2');
+        if( this.props.store.offFavorites.some(attraction => attraction.attraction_id === attractionId )){
             this.props.dispatch({type: 'TOGGLE_FAVORITE', payload: attractionId});        
         }
         // it should go here if the attraction has not been set as a favorite by
-        // the user yet
+        // the user yet to require a new post of the attraction to the favorites table
         else {
-            console.log('option3');
-            
             this.props.dispatch({type: 'SET_ATTRACTION_AS_FAVORITE', payload: attractionId});            
         }
     }
 
+    // this function will run when the user clicks 'experenced today'
     handleIncrementClick = (attractionId) => {
         console.log('ridden today clicked for attraction id', attractionId);
         // check to see if the attraction has been ridden yet and is in the attractionsQuantity
--- src/components/DateSelection/DateSelection.js
+++ src/components/DateSelection/DateSelection.js
@@ -1,23 +1,28 @@
+// DateSelectionPage component for Solo Project App for Prime Academy
+// created by Adam Boerhave, November 2020
+// This user will be brought here when they click the button on the 
+// attraction page to create a new entry.  User will select a date here,
+// then move onto dailyLogPage
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
-// Basic class component structure for React with default state
-// value setup. When making a new component be sure to replace
-// the component name TemplateClass with the name for the new
-// component.
 class DateSelection extends Component {
+    // initial state before date is selected
     state = {
         date: ''
     };
 
+    // get the id of the park the user wants to store when the page is loaded
     componentDidMount = () => {
         const { id } = this.props.match.params;
         this.props.dispatch({type: 'GET_SINGLE_PARK', payload: id});
     }
     
     // when user picks a date, take it in and make sure it is not a future date
-    handleChange = (event, input) => {
+    handleChange = (event) => {
+        // inputDate is date selected by user in the box
         let inputDate = event.target.value;
         let inputYear = inputDate.slice(0,4);
         let inputMonth = inputDate.slice(5,7);
@@ -25,14 +30,12 @@ class DateSelection extends Component {
         let inputDateToCheck = new Date(inputYear, inputMonth - 1, inputDateNumber);
         let currentDate = new Date();
         // check to make sure the date selected is not in the future
-        console.log('inputDate', inputDate);
-        console.log('inputDateToCheck', inputDateToCheck);
-        console.log('date', currentDate);
         // this one is for if they select a future date
         // it should be an error
         if (inputDateToCheck > currentDate) {
             alert('The date may not be in the future');
         }
+        // otherwise save the date in local state
         else {   
             this.setState({
                 date: inputDate
@@ -59,6 +62,8 @@ class DateSelection extends Component {
         }
         else {
             // user confirmed date is correct => send to database
+            // the park id as well as history is sent to the saga to route the 
+            // user to the dailyLog page once the post is complete
             this.props.dispatch({
                 type: 'ADD_DATE', 
                 payload: {date: inputDate, park: this.props.match.params.id}, 
@@ -74,11 +79,10 @@ class DateSelection extends Component {
                 <h2>Daily Log</h2>
                 <h2>Select a date for a visit to {this.props.store.singlePark.name}</h2>
                 <label for="parkVisitDate">Park Visit Date (MM/DD/YYYY):</label>
-                <input onChange={(event) => this.handleChange(event, 'date')} type="date" id="visitDate" name="parkVisitDate" />
+                <input onChange={(event) => this.handleChange(event)} type="date" id="visitDate" name="parkVisitDate" />
                 <button className="wordButton" onClick={this.handleSubmit}>
                     Begin Record for this Date at {this.props.store.singlePark.name}
                 </button>
-                <br/>
             </div>
         );
     }
